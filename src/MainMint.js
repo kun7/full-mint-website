9bc062885402004955f4572a91de59f5c1c0d3ca
kun7 <lanang7104@gmail.com>
2022-03-23 11:38:53 +0700

    commit to github

---
+++ src/MainMint.js
@@ -0,0 +1,61 @@
+import {useState } from 'react';
+import { ethers, BigNumber} from 'ethers';
+import roboPunkNFT from './RoboPunkNFT.json';
+
+const roboPunkNFTAddress = '0x1c5ABE1178f31355F7A6ab6550d61c2F3001Cd29';
+
+const MainMint = ({ accounts, setAccounts}) => {
+  const [mintAmount, setMintAmount] = useState(1);
+  const isConnected = Boolean(accounts[0]);
+
+  async function handleMint() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        roboPunkNFTAddress,
+        roboPunkNFT.abi,
+        signer
+      );
+      try {
+         const response = await contract.mint(BigNumber.from(mintAmount), {
+           value: ethers.utils.parseEther((0.02 * mintAmount).toString())
+         });
+         console.log('response: ', response);
+      }  catch (err) {
+         console.log('error: ', err)
+      }
+    }
+  }
+  const handleDecrement = () => {
+    if(mintAmount <= 1) return;
+    setMintAmount(mintAmount - 1);
+  };
+
+  const handleIncrement = () => {
+    if(mintAmount >= 3) return;
+    setMintAmount(mintAmount + 1);
+};
+
+return (
+  <div>
+  <h1>RoboPunk</h1>
+  <p> It's 2088. Can the Robopunks NFT save human from destructive rampant NFT speculation. Mint NFT to find out. </p>
+  {isConnected ? (
+    <div>
+      <div>
+        <button onCLick={handleDecrement}>-</button>
+        <input type="number" value={mintAmount} />
+        <button onCLick={handleIncrement}>+</button>
+      </div>
+      <button onClick={handleMint}>Mint Now</button>
+    </div>
+  ) : (
+    <p> You must be connected to Mint</p>
+  )}
+  </div>
+ );
+};
+
+
+export default MainMint;
---
+++ src/NavBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Box, Button, Flex, Image, Link, Spacer } from '@chakra-ui/react';
+import Facebook from "./assets/social-media-icons/facebook_32x32.png";
+import Twitter from "./assets/social-media-icons/twitter_32x32.png";
+import Email from "./assets/social-media-icons/email_32x32.png";
+
+const NavBar = ({ accounts, setAccounts}) => {
+  const isConnected = Boolean(accounts[0]);
+
+  async function connectAccount() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccounts(accounts);
+    }
+  }
+
+return (
+  <Flex justify="space-between" align="center" padding= "30px">
+    {/* Left Side - Social media Icons */}
+    <Flex justify="space-around" width= "40%" padding= "0 75px">Facebook</Flex>
+    <div>Twitter</div>
+    <div>Email</div>
+
+    {/*Right Side - Sections and Connect*/}
+    <div>About</div>
+    <div>Mint</div>
+    <div>Team</div>
+
+    {/*Connect*/}
+    {isConnected ? (
+      <p>Connected</p>
+    ) : (
+      <button onClick={connectAccount}>Connect</button>
+    )}
+
+  </Flex>
+ );
+};
+
+export default NavBar;
